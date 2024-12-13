597ac97c6236909bc6a6b3a49a510d0b56ba94fe
Thomas <tlux14@googlemail.com>
2024-12-13 18:51:19 +0100

    asd

---
+++ devenv.nix
@@ -0,0 +1,60 @@
+{ pkgs, lib, config, inputs, ... }:
+let
+  buildInputs = with pkgs; [
+    python312Full
+    # cudaPackages.cuda_cudart
+    # cudaPackages.cudnn
+    stdenv.cc.cc
+  ];
+
+
+in 
+{
+
+  # A dotenv file was found, while dotenv integration is currently not enabled.
+  dotenv.enable = false;
+  dotenv.disableHint = true;
+
+
+  packages = with pkgs; [
+    cudaPackages.cuda_nvcc
+    stdenv.cc.cc
+  ];
+
+  env = {
+    LD_LIBRARY_PATH = "${
+      with pkgs;
+      lib.makeLibraryPath buildInputs
+    }:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
+
+  };
+
+  languages.python = {
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+  };
+
+  scripts.hello.exec = "${pkgs.uv}/bin/uv run python hello.py";
+  
+  scripts.env-setup.exec = ''
+    export LD_LIBRARY_PATH="${
+      with pkgs;
+      lib.makeLibraryPath buildInputs
+    }:/run/opengl-driver/lib:/run/opengl-driver-32/lib"
+  '';
+
+
+  processes = {
+    # django.exec = "run-dev-server";
+    silly-example.exec = "while true; do echo hello && sleep 10; done";
+    # django.exec = "${pkgs.uv}/bin/uv run python manage.py runserver 127.0.0.1:8123";
+  };
+
+  enterShell = ''
+    . .devenv/state/venv/bin/activate
+    hello
+  '';
+}
---
+++ hello.py
@@ -0,0 +1,32 @@
+# Define a function which creates a file (YYYY-MM-DD_HH-MM-SS.txt) in the directory "./data" and writes a message to it
+
+def write_message(message=None):
+    import os
+    import datetime
+
+    # Create a directory if it does not exist
+    os.makedirs("data", exist_ok=True)
+    os.makedirs("logs", exist_ok=True)
+
+    if not message:
+        message = "SUCCESS"
+
+    # Get the current date and time
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+    # Create a file with the current date and time
+    file_name = f"logs/{date_time}.txt"
+    with open(file_name, "w") as file:
+        file.write(message)
+
+    return file_name
+
+def main():
+    print("Hello from DevEnv for endoreg-db-api project!")
+
+    write_message()
+
+
+if __name__ == "__main__":
+    main()
