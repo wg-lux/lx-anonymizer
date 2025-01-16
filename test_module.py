a3359b29982fd1df4c706e603c0361301c5b245d
maxhild <Maxhild10@gmail.com>
2025-01-16 22:11:14 +0100

    added test

--- test_module.py
+++ test_module.py
@@ -1,8 +1,7 @@
 import pytest
-
+"""
 def test_environment():
     try:
-        import os
         import sys
         import cv2
         import numpy as np
@@ -15,3 +14,9 @@ def test_environment():
     except:
         imports = False
     assert imports==True
+"""
+def test_ner():
+    from lx_anonymizer.spacy_NER import spacy_NER_German
+    text = "Herr Müller war heute in Berlin."
+    entities = spacy_NER_German(text)
+    assert entities==[('Herr Müller', 0, 10, 'PER'), ('Berlin', 20, 26, 'LOC')]
