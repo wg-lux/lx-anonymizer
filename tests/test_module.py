8e92ea5dd6b4565a2cb740199a65fc9007f8092c
maxhild <Maxhild10@gmail.com>
2025-01-24 09:50:16 +0100

    test env setup

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,4 +1,9 @@
 import pytest
+import sys
+import os
+
+# Add the project root directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
 def test_environment():
     try:
@@ -16,14 +21,14 @@ def test_environment():
     assert imports==True
 
 def test_ner():
-    from ..lx_anonymizer.spacy_NER import spacy_NER_German
+    from lx_anonymizer.spacy_NER import spacy_NER_German
     text = "Hans Müller war heute in Berlin."
     entities = spacy_NER_German(text)
     # Check if 'Hans Müller' is found as an entity
     assert any(ent[0] == "Hans Müller" and ent[3] == "PER" for ent in entities)
 
 def flair_ner():
-    from ..lx_anonymizer.flair_NER import flair_NER_German
+    from lx_anonymizer.flair_NER import flair_NER_German
     text = "Hans Müller war heute in Berlin."
     entities = flair_NER_German(text)
     # Check if 'Hans Müller' is found as an entity
