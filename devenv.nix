a4b0884f4da9fbed1d6deadba437d9cd6a529e2c
maddonix <tlux14@googlemail.com>
2025-10-14 09:52:04 +0200

    fix uv sync command in devenv.nix enter shell

--- devenv.nix
+++ devenv.nix
@@ -100,7 +100,7 @@ in
   };
 
   enterShell = ''
-    export SYNC_CMD="uv sync --extra dev --extra ocr --extra llm --extra models"
+    export SYNC_CMD="uv sync --extra dev --extra ocr --extra llm
     # uv run python env_setup.py # modifies env
        
 
